35a4500b99280d38ea3a583be3de827664c74bde
Sebastian Balzer <sebastian.balzer@gmx.ch>
2024-04-19 11:26:17 +0200

    Add read-in and code skeleton

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import os
+import pickle
 import hydra
 
 import pandas as pd
-import numpy as np
 from loguru import logger
 from omegaconf import DictConfig
 
+from utils.interpretability import Interpretability
 
-@hydra.main(version_base=None, config_path='.', config_name='config_upload')
+
+@hydra.main(version_base=None, config_path='.', config_name='config')
 def main(config: DictConfig) -> None:
-   pass
+   interpreter = Interpretability(config)
+   models_dir = config.models_dir
+   subdirs = [x[0] for x in os.walk(models_dir)][1:]  # skip the first element which is the root directory
+
+   for dir in subdirs:
+      logger.info(f'Processing {dir}')
+      train = pd.read_csv(os.path.join(dir, 'train_feature.csv'))
+      test = pd.read_csv(os.path.join(dir, 'test_feature.csv'))
+      with open(os.path.join(dir, 'model.pickle'), 'rb') as f:
+         model = pickle.load(f)
+
+      interpreter(train, test, model)
 
 
 if __name__ == '__main__':
---
+++ utils/interpretability.py
@@ -0,0 +1,9 @@
+import shap
+
+
+class Interpretability:
+    def __init__(self, config) -> None:
+        pass
+
+    def __call__(self, train, test, model) -> None:
+        pass
