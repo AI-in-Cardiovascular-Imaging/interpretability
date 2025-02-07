d95d9b043f2385a336dc3ed966dcd9c01c51787b
giovabaj <giovanni.baj2@gmail.com>
2025-02-07 15:28:15 +0100

    Sample only train. Give output path as input

--- utils/interpretability.py
+++ utils/interpretability.py
@@ -6,24 +6,22 @@ from loguru import logger
 
 
 class Interpretability:
-    def __init__(self, config) -> None:
+    def __init__(self) -> None:
         pass
 
-    def __call__(self, dir, train, test, model) -> None:
-        y_train = train['label']
-        x_train = train.drop(columns=['label'])
-        y_test = test['label']
-        x_test = test.drop(columns=['label'])
+    def __call__(self, output_path, train, test, model, label_column='label', n_train_samples=100) -> None:
+        x_train = train.drop(columns=[label_column])
+        x_test = test.drop(columns=[label_column])
 
         # sample dataset for faster runtimes
-        x_train = shap.sample(x_train, nsamples=50)
-        x_test = shap.sample(x_test, nsamples=50)
+        x_train = shap.sample(x_train, nsamples=n_train_samples)
 
         pred_function = model.predict_proba
         explainer = shap.KernelExplainer(lambda x: pred_function(x)[:, 1], x_train)
         shap_values = explainer.shap_values(x_test)
 
+        plt.figure()
         shap.summary_plot(shap_values, x_test, show=False)
         plt.tight_layout()
-        plt.savefig(os.path.join(dir, 'KernelSHAP.png'), dpi=300)
-        plt.clf()
+        plt.savefig(output_path, dpi=300)
+        plt.close()
